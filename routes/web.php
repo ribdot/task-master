e787bc08aa55ec4ee4ef4c6fbcdc92e5436f1715
ribdot <rick.fr.simon@gmail.com>
2022-09-22 14:58:45 -0700

    install tailwind

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/braille', function() {
+    return 'Braille';
+});
+
+Route::get('/morse', function() {
+    return 'Morse';
+});
